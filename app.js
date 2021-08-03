bfd55a8bdda96261510e38e920009ec1a66068c3
Matt <kuposrock@gmail.com>
2021-08-03 14:35:31 -0700

    Updated the Airline Form

--- app.js
+++ app.js
@@ -1,60 +1,24 @@
-var form = document.airlineForm;
 var submit = document.getElementById("submit");
 
 function formAlert() {
     var firstName = document.airlineForm.firstName.value;
-    console.log(firstName);
-    var lastName = form.elements["last-name"].value;
-    var age = form.elements["age"].value;
-    var gender = form.elements["gender"].value;
-    var location = form.elements["travel-location"].value;
+    var lastName = document.airlineForm.lastName.value;
+    var age = document.airlineForm.age.value;
+    var gender = document.airlineForm.gender.value;
+    var location = document.airlineForm.travelLocation.value;
     var diet = [];
-    if (form.elements['vegan'].checked) {
-        diet.push(document.getElementById("vegan").value);
+    if (document.airlineForm.vegan.checked) {
+        diet.push(document.airlineForm.vegan.value);
     }
-    if (form.elements['gluten'].checked) {
-        diet.push(document.getElementById('gluten').value);
+    if (document.airlineForm.gluten.checked) {
+        diet.push(document.airlineForm.gluten.value);
     }
-    if (form.elements['paleo'].checked) {
-        diet.push(document.getElementById('paleo').value);
+    if (document.airlineForm.paleo.checked) {
+        diet.push(document.airlineForm.paleo.value);
     }
-    alert("First Name: " + firstName + "\nLast Name: " + lastName + "\nAge: " + age + "\nGender: " + gender + "\nTravel Location: " + location + "\nDiet: " + diet + "\nAwesome, now if you die, it won't be an accident..");
+    alert("First Name: " + firstName + "\nLast Name: " + lastName + "\nAge: " + age + "\nGender: " + gender + "\nTravel Location: " + location + "\nDiet: " + diet + "\nHave a safe flight.");
 }
-
 
 submit.addEventListener("click", function(){
     formAlert();
-});
-
-
-
-
-var resultsub = document.getElementsByClassName("sub-answer");
-
-document.calcsub.addEventListener("submit", function(e){
-    e.preventDefault();
-    var subnum1 = document.calcsub.firstnum.value;
-    var subnum2 = document.calcsub.secondnum.value;
-
-    resultsub[0].innerHTML=subnum1-subnum2;
-});
-var resultadd = document.getElementsByClassName("add-answer");
-
-document.calcadd.addEventListener("submit", function(e){
-    e.preventDefault();
-    var addnum1 = document.calcadd.firstnum.value;
-    var addnum2 = document.calcadd.secondnum.value;
-
-    resultadd[0].innerHTML=parseFloat(addnum1)+parseFloat(addnum2);
-});
-
-
-var resultmul = document.getElementsByClassName("mul-answer");
-
-document.calcmul.addEventListener("submit", function(e){
-    e.preventDefault();
-    var mulnum1 = document.calcmul.firstnum.value;
-    var mulnum2 = document.calcmul.secondnum.value;
-
-    resultmul[0].innerHTML=mulnum1*mulnum2;
 });
